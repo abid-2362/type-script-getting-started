0812e79a9c56e88dcbd6221d4b9da861beba87d9
Abid Ali <abdsoftfsd@gmail.com>
2020-11-07 05:30:23 +0500

    Course completed

---
+++ app/scoreboard.ts
@@ -0,0 +1,23 @@
+import { Result } from "./result";
+import * as _ from "lodash";
+
+export class Scoreboard {
+  private results: Result[] = [];
+  
+  addResult(newResult: Result): void {
+    this.results.push(newResult);
+    let allCpasName: string = _.upperCase(newResult.playerName);
+    console.log(`${allCpasName}: ${newResult.score}`);
+  }
+  
+  updateScorboard(): void {
+    let output: string = `<h2>Scoreboard</h2>`;
+    
+    for ( let index = 0; index < this.results.length; index++ ) {
+      const result: Result = this.results[ index ];
+      output += `<h4>${result.playerName} : ${result.score}/${result.problemCount} for factor ${result.factor}</h4>`;
+      const scoresElement: HTMLElement = document.getElementById('scores')!;
+      scoresElement.innerHTML = output;
+    }
+  }
+}
